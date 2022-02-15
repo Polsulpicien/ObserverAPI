9c0e08143a528d882fc40bd0d1ef30894fd7ccbd
Polsulpicien <70817001+Polsulpicien@users.noreply.github.com>
2022-02-15 19:53:50 +0100

    Update __init__.py

--- ObserverAPI/__init__.py
+++ ObserverAPI/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "ObserverAPI"
 __description__ = "Observer API Wrapper in Python"
 __author__ = "Polsulpicien"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 from .client import Observer
 from .exception import *
