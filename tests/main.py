b6536e9844df6ac790fda6ffe1c590a6f000806a
Polsulpicien <70817001+Polsulpicien@users.noreply.github.com>
2022-02-22 13:36:22 +0100

    Update main.py

--- tests/main.py
+++ tests/main.py
@@ -1,8 +1,11 @@
-from ObserverAPI import Observer
 import asyncio
 
+from ObserverAPI import Observer
+
+key="INSERT YOUR OBSERVER API KEY HERE" #Observer API Key
+
 async def main():
-    client = await Observer() #define the client
+    client = await Observer(key=key) #define the client
 
     # Get the stats of Technoblade (UUID: b876ec32e396476ba1158438d83c67d4) 100 days ago
     lookback = await client.lookback.get("b876ec32e396476ba1158438d83c67d4", 100)
@@ -10,7 +13,7 @@ async def main():
     
     # Observer API Stats
     api_stats = await client.stats.get()
-    print(api_stats.trackedPlayers)
+    print(api_stats.tracked_players)
     
     # Get the Top 10 players, All-Time Bedwars Wins Leaderboard 
     leaderboard = await client.leaderboard.get("alltime", "bedwars", "wins", 1, 10)
@@ -21,11 +24,14 @@ async def main():
     print(second_player_on_leaderboard.formatted, second_player_on_leaderboard.rank, second_player_on_leaderboard.value)
 
     # Get Technoblade (UUID: b876ec32e396476ba1158438d83c67d4) leaderboard rank, in this case in the All-Time Bedwars Wins Leaderboard
-    ranking = await client.ranking.get("b876ec32e396476ba1158438d83c67d4", "alltime", "bedwars", "wins")
-    print(ranking.rank)
-    
+    player_ranking = await client.ranking.get("b876ec32e396476ba1158438d83c67d4", "alltime", "bedwars", "wins")
+    print(player_ranking.rank)
+
     # Get the formatted name of a Player, in this case, Technoblade (UUID: b876ec32e396476ba1158438d83c67d4) -> §d[PIG§b+++§d] Technoblade
     formatted = await client.formatted.get("b876ec32e396476ba1158438d83c67d4")
     print(formatted.formatted)
+
+    daily = await client.daily.get("b876ec32e396476ba1158438d83c67d4")
+    print(daily.bedwars.wins)
 
 asyncio.get_event_loop().run_until_complete(main()) # launch the main function
